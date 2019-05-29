83961038bdf1ff5f506f8da65926f8cf3c8b78e0
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-29 22:32:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,3 +16,8 @@ def assign_rooms(attendees)
    end
    array
 end
+
+def printer(attendees)
+   batch_badge_creator(attendees).each { |names| puts names }
+   assign_rooms(attendees).each { |room| puts room }
+end
